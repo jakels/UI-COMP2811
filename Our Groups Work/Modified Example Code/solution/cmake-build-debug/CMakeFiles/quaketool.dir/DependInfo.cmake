dc02da704981ea9ef9846d6a96d9098ed85d9c75
VH <ll22jls@leeds.ac.uk>
2024-11-26 12:53:51 +0000

    Reorganised

---
+++ Our Groups Work/Modified Example Code/solution/cmake-build-debug/CMakeFiles/quaketool.dir/DependInfo.cmake
@@ -0,0 +1,30 @@
+
+# Consider dependencies only in project.
+set(CMAKE_DEPENDS_IN_PROJECT_ONLY OFF)
+
+# The set of languages for which implicit dependencies are needed:
+set(CMAKE_DEPENDS_LANGUAGES
+  )
+
+# The set of dependency files which are needed:
+set(CMAKE_DEPENDS_DEPENDENCY_FILES
+  "" "quaketool_autogen/timestamp" "custom" "quaketool_autogen/deps"
+  "/uolstore/home/users/ll22jls/UI-COMP2811/cwk2_solution_final/solution/dataset.cpp" "CMakeFiles/quaketool.dir/dataset.cpp.o" "gcc" "CMakeFiles/quaketool.dir/dataset.cpp.o.d"
+  "/uolstore/home/users/ll22jls/UI-COMP2811/cwk2_solution_final/solution/main.cpp" "CMakeFiles/quaketool.dir/main.cpp.o" "gcc" "CMakeFiles/quaketool.dir/main.cpp.o.d"
+  "/uolstore/home/users/ll22jls/UI-COMP2811/cwk2_solution_final/solution/model.cpp" "CMakeFiles/quaketool.dir/model.cpp.o" "gcc" "CMakeFiles/quaketool.dir/model.cpp.o.d"
+  "/uolstore/home/users/ll22jls/UI-COMP2811/cwk2_solution_final/solution/quake.cpp" "CMakeFiles/quaketool.dir/quake.cpp.o" "gcc" "CMakeFiles/quaketool.dir/quake.cpp.o.d"
+  "/uolstore/home/users/ll22jls/UI-COMP2811/cwk2_solution_final/solution/cmake-build-debug/quaketool_autogen/mocs_compilation.cpp" "CMakeFiles/quaketool.dir/quaketool_autogen/mocs_compilation.cpp.o" "gcc" "CMakeFiles/quaketool.dir/quaketool_autogen/mocs_compilation.cpp.o.d"
+  "/uolstore/home/users/ll22jls/UI-COMP2811/cwk2_solution_final/solution/stats.cpp" "CMakeFiles/quaketool.dir/stats.cpp.o" "gcc" "CMakeFiles/quaketool.dir/stats.cpp.o.d"
+  "/uolstore/home/users/ll22jls/UI-COMP2811/cwk2_solution_final/solution/window.cpp" "CMakeFiles/quaketool.dir/window.cpp.o" "gcc" "CMakeFiles/quaketool.dir/window.cpp.o.d"
+  )
+
+# Targets to which this target links which contain Fortran sources.
+set(CMAKE_Fortran_TARGET_LINKED_INFO_FILES
+  )
+
+# Targets to which this target links which contain Fortran sources.
+set(CMAKE_Fortran_TARGET_FORWARD_LINKED_INFO_FILES
+  )
+
+# Fortran module output directory.
+set(CMAKE_Fortran_TARGET_MODULE_DIR "")
